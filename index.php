48b0336d731c520637edaf95d9f7dc1a0931feea
tsommie <anitchec.dev@gmail.com>
2019-03-26 11:08:23 +0000

    Add index.php.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+define('MPPHP_ROOT', __DIR__. '/');
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our files later on. It feels great to relax.
+|
+*/
+
+require MPPHP_ROOT . 'vendor/autoload.php';
